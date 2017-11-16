90568a7fc411fbc2999463b4268f366d3812ebf4
fenghaoran <harrifeng@gmail.com>
2017-11-16 12:50:13 +0000

    new include

--- 001-ab.sh
+++ 001-ab.sh
@@ -1,1 +1,5 @@
-docker run -v $(pwd):/code --rm jordi/ab ab -c10 -n 50000 -p /code/batch.txt http://192.168.161.44:5000/QukanLog
+for i in {1..200}
+do
+    docker run -v $(pwd):/code --rm jordi/ab ab -c10 -n 5000 -p /code/batch.txt http://192.168.161.44:5000/QukanLog
+    sleep 2
+done
---
+++ 003-container-top.sh
@@ -0,0 +1,2 @@
+# !/bin/bash
+docker run --rm -ti --name=ctop -v /var/run/docker.sock:/var/run/docker.sock quay.io/vektorlab/ctop:latest
